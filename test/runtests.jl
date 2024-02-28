ac2a90a70b7569ba46435349786cc75db5cc11de
Tianyi Pu <912396513@qq.com>
2024-02-28 19:04:59 +0000

    add a test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,18 @@
 using PseudostableRecurrences
 using Test
 
-@testset "PseudostableRecurrences.jl" begin
+@testset "Examples" begin
     # Write your tests here.
+    @testset "Linear recurrence sequence" begin
+        stencil = (CartesianIndex(-3), CartesianIndex(-2), CartesianIndex(-1))
+        coefs = (n -> 2//3, n -> -3, n -> 10//3)
+        f_init(T) = [sqrt(T(2)), sqrt(T(2)), sqrt(T(2)), T(0)]
+        P = StencilRecurrencePlan{Real}(stencil, coefs, f_init, (100,))
+        @test stable_recurrence(P)[end] ≈ sqrt(2)
+    end
+end
+
+@testset "Extensions" begin
     @testset "Product" begin
         using PseudostableRecurrences: Product
         A = Product(1:2,1:3)
