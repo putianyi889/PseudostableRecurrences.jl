80c52d7acb093645aa7a9a2cd30286f2e1eb55ce
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2024-02-29 14:45:52 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -13,5 +13,5 @@ makedocs(
 deploydocs(
     repo = "https://github.com/putianyi889/PseudostableRecurrences.jl.git",
     devurl = "dev",
-    versions = ["stable" => "v^", "v0.0.1" => "v0.0.1-doc2", "v#.#.#", "dev" => "dev"]
+    versions = ["stable" => "v^", "v0.0.1" => "v0.0.1-doc2", "v0.0.2" => "v0.0.2-doc2", "v#.#.#", "dev" => "dev"]
 )
