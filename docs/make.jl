634ea3332162873877a3e185f3aaa796c844b61b
Tianyi Pu <912396513@qq.com>
2024-02-29 16:44:11 +0000

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -11,7 +11,7 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "https://github.com/putianyi889/PseudostableRecurrences.jl.git",
+    repo = "github.com/putianyi889/PseudostableRecurrences.jl.git",
     devurl = "dev",
     versions = ["stable" => "v^", "v0.0.1" => "v0.0.1-doc2", "v0.0.2" => "v0.0.2-doc2", "v#.#.#", "dev" => "dev"]
 )
