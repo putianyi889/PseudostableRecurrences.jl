87d8b03a46d72f8355a651796eb356c8914ac197
Tianyi Pu <912396513@qq.com>
2024-07-28 15:04:30 +0100

    Revert "update"
    This reverts commit 49efae270aeaae5d2ab0d85e71c431b99b8d1153.

--- ext/PseudostableRecurrencesQuasiArraysExt.jl
+++
@@ -1,8 +0,0 @@
-module PseudostableRecurrencesQuasiArraysExt
-
-import QuasiArrays: AbstractQuasiArray
-import EltypeExtensions: elconvert
-
-elconvert(::Type{T}, A::AbstractQuasiArray) where T = AbstractQuasiArray{T}(A)
-
-end # module
